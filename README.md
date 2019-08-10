7572091a020a45def61a1ce036c7576d238460e3
Pookage <ahoy@pookage.dev>
2019-08-10 13:44:25 +0100

    initialised repo from a copy of bundled-web-components

---
+++ README.md
@@ -0,0 +1,8 @@
+# Menu Button with Web Components
+
+A Menu Button component with open/closed state made with:
+
+- Web Components
+- CSS Modules
+- SCSS
+- Webpack
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path   = require('path');
+const src    = path.resolve(__dirname, "src");
+const dist   = path.resolve(__dirname, "dist");
+function buildConfig(env, args){
+
+	const { mode } = args;
+
+	let modeOptions;
+	switch(mode){
+		case "development":
+			modeOptions = {
+				mode: "development",
+				devServer: {
+					contentBase: "./dist"
+				},
+				output: {
+					filename: "bundle.js",
+					path: dist,
+					publicPath: "/"
+				},
+			};
+			break;
+		case "production":
+		default:
+			modeOptions = {
+				mode: "production",
+				output: {
+					filename: "bundle.js",
+					path: dist,
+					publicPath: "./"
+				}
+			};
+			break;
+	}
+
+	return {
+		entry: `${src}/index.js`,
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								modules: {
+									localIdentName: "[folder]__[local]",
+								},
+								url: false
+							}
+						}, {
+							loader: "sass-loader"
+						}
+					]	
+				},
+			]
+		},
+		...modeOptions,
+	}
+}
+
+module.exports = buildConfig;
